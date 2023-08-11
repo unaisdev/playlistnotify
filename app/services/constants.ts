55e08a334b04ec4f3dd6cfae198f556e7e63b712
Unai Canales <unaicanales@Mac-mini-de-Unai.local>
2023-08-11 02:58:31 +0200

    api_url constant changed

--- app/services/constants.ts
+++ app/services/constants.ts
@@ -1,4 +1,4 @@
-export const API_URL = 'http://192.168.1.128:3000';
+export const API_URL = 'http://192.168.1.145:3000';
 export const SPOTIFY_API_URL = 'https://api.spotify.com/v1';
 
 export const SEARCH_TYPE = {
