953814f2542b4a64f0c6af6a0ff32d4ec0c1c330
Unai Canales <unaicanales@Mac-mini-de-Unai.local>
2023-08-22 00:17:43 +0200

    playlist tracks items updated

--- app/screens/playlist/components/TrackItem/index.tsx
+++ app/screens/playlist/components/TrackItem/index.tsx
@@ -7,14 +7,54 @@ interface Props {
   item: PlaylistItem;
 }
 
+const formatDuration = (duration: number) => {
+  const minutes = Math.floor(duration / 60000); // Divide by 60000 to get minutes
+  const seconds = Math.floor((duration % 60000) / 1000); // Get the remaining seconds
+
+  // Format the minutes and seconds as two-digit numbers
+  const formattedMinutes = String(minutes).padStart(2, '0');
+  const formattedSeconds = String(seconds).padStart(2, '0');
+
+  return `${formattedMinutes}:${formattedSeconds}`;
+};
+
 const TrackItem = ({item}: Props) => {
+  const artists = item.track.artists.map((artist, index) => {
+    // Capitalize the first letter of each artist name
+    const formattedName =
+      artist.name.charAt(0).toUpperCase() + artist.name.slice(1);
+
+    // Separate artists with commas, except for the last one
+    const separator = index === item.track.artists.length - 1 ? '' : ', ';
+
+    return (
+      <View key={artist.id}>
+        <Text style={{fontSize: 10}}>
+          {formattedName}
+          {separator}
+        </Text>
+      </View>
+    );
+  });
+
   return (
     <View style={styles.item}>
-      <Image
-        source={{uri: item.track.album.images[0]?.url ?? ''}}
-        style={styles.image}
-      />
-      <Text>{item.track.name}</Text>
+      <View style={styles.inline}>
+        <Image
+          source={{uri: item.track.album.images[0]?.url ?? ''}}
+          style={styles.image}
+        />
+        <View style={{gap: 4}}>
+          <Text>{item.track.name}</Text>
+          <Text>{artists}</Text>
+        </View>
+      </View>
+
+      <View>
+        <Text style={{fontSize: 12}}>
+          {formatDuration(item.track.duration_ms)}
+        </Text>
+      </View>
     </View>
   );
 };
@@ -29,7 +69,12 @@ const styles = StyleSheet.create({
     marginVertical: 4,
     paddingHorizontal: 6,
   },
-  inline: {},
+  inline: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 6,
+  },
   image: {
     width: 46,
     height: 46,
