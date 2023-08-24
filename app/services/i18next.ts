10fdca74953233b7b486e805f9e8f0f44602cc7d
unaicanales <36845145+Seretoiden@users.noreply.github.com>
2023-08-24 03:43:01 +0200

    import deleted

--- app/services/i18next.ts
+++ app/services/i18next.ts
@@ -1,6 +1,5 @@
 import i18n from 'i18next';
 import {initReactI18next} from 'react-i18next';
-import Backend from 'i18next-http-backend';
 
 import en from '../features/locales/en.json';
 import es from '../features/locales/es.json';
