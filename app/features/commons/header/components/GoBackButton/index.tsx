bf86876f739891a4d31ca2b896bae924408a7a63
unaicanales <unaicanales@gmail.com>
2023-08-25 18:54:10 +0200

    goBackButton folder structure update

---
+++ app/features/commons/header/components/GoBackButton/index.tsx
@@ -0,0 +1,30 @@
+import {Image, StyleSheet, TouchableOpacity, View} from 'react-native';
+import Text from '@app/features/commons/layout/Text';
+
+import {NativeStackNavigationProp} from '@react-navigation/native-stack';
+import {RootStackParamList} from '../../../../../navigation';
+
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+
+import {useNavigation} from '@react-navigation/native';
+
+interface Props {}
+
+const GoBackButton = ({}: Props) => {
+  const navigation =
+    useNavigation<NativeStackNavigationProp<RootStackParamList>>();
+
+  const handlePress = () => {
+    navigation.goBack();
+  };
+
+  if (!navigation.canGoBack()) return;
+
+  return (
+    <TouchableOpacity style={{height: 28}} onPress={handlePress}>
+      <MaterialCommunityIcons name="arrow-left" size={22} />
+    </TouchableOpacity>
+  );
+};
+
+export default GoBackButton;
