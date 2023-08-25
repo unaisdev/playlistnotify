efafc09a2bf2ff9f1862e326c5f6492acbe69a73
unaicanales <unaicanales@gmail.com>
2023-08-25 16:44:31 +0200

    cleanning component profile

--- app/screens/profile/index.tsx
+++ app/screens/profile/index.tsx
@@ -15,15 +15,10 @@ const ProfileScreen = () => {
   const {user} = useUserContext();
 
   const [userPlaylists, setUserPlaylists] = useState<PlaylistModel[]>();
-  const [userFeaturedPlaylists, setUserFeaturedPlaylists] = useState<
-    PlaylistModel[]
-  >([]);
 
   const init = async () => {
     const userPlaylists = await getUserPlaylists();
-    // const userFeaturedPlaylists = await getUserFeaturedPlaylists();
     setUserPlaylists(userPlaylists);
-    // setUserFeaturedPlaylists(userFeaturedPlaylists);
   };
 
   useEffect(() => {
@@ -31,53 +26,34 @@ const ProfileScreen = () => {
   }, []);
 
   if (!user) return;
-  // if (!userPlaylists) {
-  //   return (
-  //     <View>
-  //       <Text>Loading...</Text>
-  //     </View>
-  //   );
-  // }
 
   return (
-    <View style={{backgroundColor: 'black', height: '100%'}}>
-      <View style={styles.container}>
-        <View style={{padding: 12}}>
-          <Text
-            style={{
-              fontSize: 12,
-              color: 'white',
-            }}>
-            Tanto aquí, como en el buscador, podrás elegir las listas de las que
-            quieres recibir notificaciones en Spotify.
-          </Text>
-        </View>
-
-        {/* <View>
-        <SavedSpotifyLists
-          text={'Últimas listas vistas'}
-          userOwnedPlaylists={userFeaturedPlaylists}
-        />
-      </View> */}
-
-        <View>
-          <SavedSpotifyLists
-            text={'Tus listas'}
-            playlists={userPlaylists?.filter(item =>
-              item.owner.display_name.includes(user.display_name),
-            )}
-          />
-        </View>
-
-        <View>
-          <SavedSpotifyLists
-            text={'Tus listas guardadas'}
-            playlists={userPlaylists?.filter(
-              item => !item.owner.display_name.includes(user.display_name),
-            )}
-          />
-        </View>
+    <View
+      style={[styles.container, {backgroundColor: 'black', height: '100%'}]}>
+      <View style={{padding: 12}}>
+        <Text
+          style={{
+            fontSize: 12,
+            color: 'white',
+          }}>
+          Tanto aquí, como en el buscador, podrás elegir las listas de las que
+          quieres recibir notificaciones en Spotify.
+        </Text>
       </View>
+
+      <SavedSpotifyLists
+        text={'Tus listas'}
+        playlists={userPlaylists?.filter(item =>
+          item.owner.display_name.includes(user.display_name),
+        )}
+      />
+
+      <SavedSpotifyLists
+        text={'Tus listas guardadas'}
+        playlists={userPlaylists?.filter(
+          item => !item.owner.display_name.includes(user.display_name),
+        )}
+      />
     </View>
   );
 };
@@ -86,7 +62,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
-  topinfo: {},
 });
 
 export default ProfileScreen;
