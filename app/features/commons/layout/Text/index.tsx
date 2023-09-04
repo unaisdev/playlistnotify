db6eb93b676915c6ac54de10df2b0b7175f4ddfb
unaicanales <unaicanales@gmail.com>
2023-09-04 23:54:24 +0200

    can revert now text component color

--- app/features/commons/layout/Text/index.tsx
+++ app/features/commons/layout/Text/index.tsx
@@ -4,12 +4,20 @@ import {useTheme} from '../../theme/hooks/useTheme';
 
 interface CustomTextProps extends TextProps {
   textType?: 'regular' | 'bold' | 'light';
+  colorReverted?: boolean;
 }
 
 type Props = CustomTextProps & PropsWithChildren;
 
-const Text: React.FC<Props> = ({children, textType, style: comingStyles}) => {
+const Text: React.FC<Props> = ({
+  children,
+  textType,
+  style: comingStyles,
+  colorReverted,
+}) => {
   const {isDarkMode} = useTheme();
+
+  const styles = styling(isDarkMode);
 
   const style = useMemo(() => {
     if (textType === 'bold') return styles.bold;
@@ -17,26 +25,35 @@ const Text: React.FC<Props> = ({children, textType, style: comingStyles}) => {
     if (textType === 'regular') return styles.regular;
   }, [textType]);
 
+  const colorRevertedStyle = useMemo(() => {
+    if (colorReverted) return styles.revertedTextColor;
+  }, [colorReverted]);
+
   return (
-    <RNText style={[styles.textColor(isDarkMode), style, comingStyles]}>
+    <RNText style={[styles.textColor, style, comingStyles, colorRevertedStyle]}>
       {children}
     </RNText>
   );
 };
 
-const styles = StyleSheet.create({
-  textColor: (isDarkMode: boolean) => ({
-    color: isDarkMode ? 'white' : 'black',
-  }),
-  regular: {
-    fontWeight: '500',
-  },
-  bold: {
-    fontWeight: 'bold',
-  },
-  light: {
-    fontWeight: '200',
-  },
-});
+const styling = (isDarkMode: boolean) => {
+  return StyleSheet.create({
+    textColor: {
+      color: isDarkMode ? 'white' : 'black',
+    },
+    revertedTextColor: {
+      color: isDarkMode ? 'black' : 'white',
+    },
+    regular: {
+      fontWeight: '500',
+    },
+    bold: {
+      fontWeight: 'bold',
+    },
+    light: {
+      fontWeight: '200',
+    },
+  });
+};
 
 export default Text;
