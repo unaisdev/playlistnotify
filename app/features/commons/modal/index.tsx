5ef2aa98a7f8307a0098038de056f55eac8ad63a
unaicanales <unaicanales@gmail.com>
2023-09-13 22:39:20 +0200

    modal UI updated

--- app/features/commons/modal/index.tsx
+++ app/features/commons/modal/index.tsx
@@ -2,6 +2,7 @@ import {useState} from 'react';
 import {Alert, View, Pressable, StyleSheet, Modal} from 'react-native';
 import Text from '@app/features/commons/layout/Text';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import Layout from '../layout/TabLayout';
 
 type Props = {
   modalVisible: boolean;
@@ -19,20 +20,63 @@ const AppModal = ({modalVisible, toggleModal}: Props) => {
         toggleModal();
       }}>
       <View style={styles.centeredView}>
-        <View style={styles.modalView}>
+        <View style={{width: '70%', alignItems: 'flex-end', paddingTop: 20}}>
           <Pressable
             style={[styles.button, styles.buttonClose]}
             onPress={() => toggleModal()}>
             <MaterialCommunityIcons name="close" size={22} color={'black'} />
           </Pressable>
-          <Text style={styles.modalText}>
-            ¡Atención! Esta aplicación está en fase beta.
-          </Text>
+        </View>
+
+        <Layout style={[styles.modalView, {flex: 0, padding: 20}]}>
+          <View
+            style={{
+              width: '100%',
+              flexDirection: 'row',
+              alignItems: 'center',
+              justifyContent: 'space-between',
+            }}>
+            <View
+              style={{
+                flex: 1,
+                borderRadius: 12,
+                flexDirection: 'row',
+                paddingRight: 12,
+                alignItems: 'center',
+                backgroundColor: '#4CAF50',
+                justifyContent: 'space-between',
+              }}>
+              <MaterialCommunityIcons
+                name="beta"
+                size={22}
+                color={'black'}
+                style={{padding: 8}}
+              />
+              <Text colorReverted>BETA Release</Text>
+            </View>
+          </View>
+
           <Text style={styles.modalText}>
             Comentar que podrían ocurrir fallos en cualquier momento. Gracias
             por tu comprensión y paciencia.
           </Text>
-        </View>
+          <View
+            style={{
+              width: '100%',
+              justifyContent: 'center',
+              alignItems: 'center',
+              rowGap: 18,
+            }}>
+            <Text style={{opacity: 0.7, fontSize: 14}}>
+              Made by Unai Canales with {`<3`}
+            </Text>
+            <View style={{alignItems: 'center'}}>
+              <Text style={{opacity: 0.7, fontSize: 12}}>Playlist Notify</Text>
+              <Text style={{opacity: 0.7, fontSize: 12}}>-</Text>
+              <Text style={{opacity: 0.7, fontSize: 12}}>Versión ß 1.0</Text>
+            </View>
+          </View>
+        </Layout>
       </View>
     </Modal>
   );
@@ -46,31 +90,32 @@ const styles = StyleSheet.create({
   },
   modalView: {
     width: '80%',
-    margin: 20,
-    backgroundColor: '#00C853',
     borderRadius: 20,
-    padding: 20,
+    paddingHorizontal: 20,
+    paddingVertical: 20,
     justifyContent: 'space-between',
-    alignItems: 'center',
+    alignItems: 'flex-start',
     shadowColor: '#000',
     shadowOffset: {
       width: 10,
       height: 20,
     },
-    shadowOpacity: 0.25,
+    shadowOpacity: 0.1,
     shadowRadius: 4,
-    elevation: 5,
+    elevation: 20,
+    rowGap: 12,
   },
   button: {
-    borderRadius: 8,
-    padding: 10,
+    borderTopRightRadius: 8,
+    borderTopLeftRadius: 8,
+    padding: 4,
     elevation: 2,
   },
   buttonOpen: {
     backgroundColor: '#F194FF',
   },
   buttonClose: {
-    backgroundColor: '#2196F3',
+    backgroundColor: '#F4511E',
   },
   textStyle: {
     color: 'white',
@@ -78,8 +123,7 @@ const styles = StyleSheet.create({
     textAlign: 'center',
   },
   modalText: {
-    marginBottom: 15,
-    textAlign: 'center',
+    textAlign: 'left',
   },
 });
 
