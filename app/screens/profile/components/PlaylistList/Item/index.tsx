72b73dab33b9fa2bdd84643441dad08cb68de957
unaicanales <unaicanales@gmail.com>
2023-09-27 03:27:33 +0200

    profile list Square item display

--- app/screens/profile/components/PlaylistList/Item/index.tsx
+++ app/screens/profile/components/PlaylistList/Item/index.tsx
@@ -17,78 +17,98 @@ import {useTheme} from '@app/features/commons/theme/hooks/useTheme';
 import {NativeStackNavigationProp} from '@react-navigation/native-stack';
 
 type Props = {
+  selectedDisplay: string;
   item: PlaylistModel;
   index: number;
 };
 
-const PlaylistItem = ({item, index}: Props) => {
+const PlaylistItem = ({item, index, selectedDisplay}: Props) => {
   const {isDarkMode} = useTheme();
   const {t} = useTranslation();
   const navigation =
     useNavigation<NativeStackNavigationProp<RootStackParamList>>();
-
   return (
     <Animated.View
       key={item.id}
       entering={FadeInDown.duration(300).delay(index * 25)}
       exiting={FadeOutRight.duration(300)}
-      layout={Layout.duration(800).delay(400)}>
+      layout={Layout.duration(800).delay(400)}
+      style={{flex: 1}}>
       <Pressable
         onPress={() => {
           navigation.navigate('Playlist', {id: item.id});
         }}
         key={index}
-        style={{
-          flex: 1,
-          flexDirection: 'row',
-          alignItems: 'center',
-          justifyContent: 'flex-start',
-          width: '100%',
-          gap: 12,
-        }}>
-        <Image
-          style={{width: 58, height: 58}}
-          source={{uri: item.images[0]?.url}}
-        />
+        style={
+          selectedDisplay === 'square' && {
+            width: selectedDisplay === 'square' ? '100%' : 58,
+            flex: 1,
+            paddingHorizontal: 12,
+            columnGap: 10,
+          }
+        }>
         <View
           style={{
-            flexGrow: 1,
-            flexDirection: 'column',
-            justifyContent: 'center',
+            flexDirection: selectedDisplay === 'square' ? 'column' : 'row',
+            justifyContent: 'flex-start',
+            columnGap: 12,
+            rowGap: 10,
           }}>
-          <View>
-            <Text textType="bold" numberOfLines={2} style={{width: 260}}>
+          <Image
+            resizeMode={'contain'}
+            style={{
+              width: selectedDisplay === 'square' ? '100%' : 58,
+              aspectRatio: 1,
+            }}
+            source={{uri: item.images[0]?.url}}
+          />
+          {selectedDisplay === 'square' ? null : ( // Ocultar imagen en 'square'
+            <View
+              style={{
+                flexGrow: 1,
+                flexDirection: 'column',
+                justifyContent: 'center',
+              }}>
+              <View>
+                <Text textType="bold" numberOfLines={2} style={{width: 260}}>
+                  {item.name}
+                </Text>
+              </View>
+
+              <View
+                style={{
+                  flexDirection: 'row',
+                  justifyContent: 'space-between',
+                }}>
+                <Text textType="light" style={{fontSize: 10}}>
+                  {item.tracks.total} {t('tracks')}
+                </Text>
+                <Pressable
+                  onPress={() => {
+                    Linking.openURL(item.owner.uri);
+                  }}
+                  style={{
+                    flexDirection: 'row',
+                    alignItems: 'center',
+                    gap: 4,
+                  }}>
+                  <Feather
+                    name="user"
+                    size={8}
+                    color={isDarkMode ? 'white' : 'black'}
+                  />
+                  <Text textType="regular" style={{fontSize: 10}}>
+                    {item.owner?.display_name}
+                  </Text>
+                </Pressable>
+              </View>
+            </View>
+          )}
+          {selectedDisplay === 'square' && ( // Mostrar solo el nombre en 'square'
+            <Text textType="semi" numberOfLines={1}>
               {item.name}
             </Text>
-          </View>
-
-          <View
-            style={{
-              flexDirection: 'row',
-              justifyContent: 'space-between',
-            }}>
-            <Text textType="light" style={{fontSize: 10}}>
-              {item.tracks.total} {t('tracks')}
-            </Text>
-            <Pressable
-              onPress={() => {
-                Linking.openURL(item.owner.uri);
-              }}
-              style={{
-                flexDirection: 'row',
-                alignItems: 'center',
-                gap: 4,
-              }}>
-              <Feather
-                name="user"
-                size={8}
-                color={isDarkMode ? 'white' : 'black'}
-              />
-              <Text textType="regular" style={{fontSize: 10}}>
-                {item.owner?.display_name}
-              </Text>
-            </Pressable>
-          </View>
+          )}
         </View>
       </Pressable>
     </Animated.View>
