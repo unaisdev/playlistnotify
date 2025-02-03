5662434d9bb3207cb3cd53b43f24994cefb6623e
unaisdev <unaicanales@gmail.com>
2025-02-03 22:13:14 +0100

    refactor queryClient and create queryKeys constants

---
+++ app/lib/queryKeys.ts
@@ -0,0 +1,17 @@
+export const QUERY_KEYS = {
+  // User related keys
+  USER_PROFILE: 'userProfile',
+  USER_PLAYLISTS: 'userPlaylists',
+  USER_NOTIFIED_PLAYLISTS: 'userNotifiedPlaylists',
+
+  // Track related keys
+  TRACK_DETAIL: 'trackDetail',
+
+  // Search related keys
+  SEARCH_PLAYLISTS: 'searchPlaylists',
+
+  // Playlist related keys
+  PLAYLIST_DETAIL: 'playlistDetail',
+  PLAYLIST_TRACKS: 'playlistTracks',
+  PLAYLIST_IS_SAVED: 'playlistIsSaved',
+} as const; 
